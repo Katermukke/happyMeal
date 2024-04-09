98cc7960a5448b8da023d942eb82e5223a656555
arhtur-clerc <arthur.clerc@laplateforme.io>
2024-04-09 10:16:03 +0200

    maj arthur main

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const slides = document.querySelectorAll(".slide");
-  let index = 0;
 
-  setInterval(() => {
-    index = (index + 1) % slides.length;
-    document.querySelector(".slides").style.transform = `translateX(-${
-      index * 100
-    }%)`;
-  }, 5000); // Changement d'arrière-plan toutes les 5 secondes
-});
