32878a52c4b8e3ebdda39b6a6f7f42fc905f08cf
jonathan-le-plat <jonathan.le-plat@laplateforme.io>
2024-04-10 11:21:19 +0200

    add Todolist function for ingredients

--- recipes.js
+++ recipes.js
@@ -1,5 +1,5 @@
 let currentPage = 1;
-const recipesPerPage = 9;
+let recipesPerPage = 9;
 
 function displayRecipes(page) {
     fetch('data.json')
@@ -24,8 +24,13 @@ function displayRecipes(page) {
                         <p>${recipe.categorie}</p>
                         <p>Temps de préparation : ${recipe.temps_preparation}</p>
                         <ul>
-                            ${recipe.ingredients.map(ingredient => `<li class="ingredient">${ingredient.nom}: ${ingredient.quantite}
-                            <button class="addBtn">+</button></li>`).join('')}
+                            ${recipe.ingredients.map(ingredient => `
+                                <li class="ingredient">
+                                    <span class="nom">${ingredient.nom}:</span>
+                                    <span class="quantite">${ingredient.quantite}</span>
+                                    <button class="addBtn">+</button>
+                                </li>
+                            `).join('')}
                         </ul>
                     </div>
                 </div>
@@ -37,6 +42,9 @@ function displayRecipes(page) {
             `;
         });
 
+        attachEventListeners();
+    })
+    .then(() => {
         if (page === 1) {
             currentPage = 2;
         }
@@ -47,6 +55,7 @@ function displayRecipes(page) {
         }
     });
 }
+
 
 function initPagination() {
     const paginationControls = document.querySelector('#pagination-controls');
@@ -62,21 +71,49 @@ initPagination();
 
 ///////////
 
-fetch('data.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error
-                (`HTTP error! Status: ${response.status}`);
-            } 
-                return response.json();
-            })    
-    .then(data => {
-        const dataString = JSON.stringify(data);
-        localStorage.setItem('favRecipes', dataString);
-        console.log('Toutes mes recettes');
-        })
-    .catch((error) => 
-        console.error("Unable to fetch data:", error));
+// fetch('data.json')
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error
+//                 (`HTTP error! Status: ${response.status}`);
+//             } 
+//                 return response.json();
+//             })    
+//     .then(data => {
+//         const dataString = JSON.stringify(data);
+//         localStorage.setItem('favRecipes', dataString);
+//         console.log('Toutes mes recettes');
+//         })
+//     .catch((error) => 
+//         console.error("Unable to fetch data:", error));
 
+function attachEventListeners() {
+    const addBtns = document.querySelectorAll(".addBtn");
+    addBtns.forEach(btn => {
+        btn.addEventListener('click', function () {
 
-        
+            const ingredient = this.parentNode;
+            const nom = ingredient.querySelector('.nom').textContent;
+            const quantite = parseInt(ingredient.querySelector('.quantite').textContent);
+
+            let existingList = localStorage.getItem('List');
+            if (!existingList) {
+                existingList = [];
+            } else {
+                existingList = JSON.parse(existingList);
+            }
+
+            const existingIngredient = existingList.find(item => item.nom === nom);
+
+            if (existingIngredient) {
+                existingIngredient.quantite += quantite;
+            } else {
+                existingList.push({ nom: nom, quantite: quantite });
+            }
+
+            localStorage.setItem('List', JSON.stringify(existingList));
+        });
+    });
+}
+
+attachEventListeners();
