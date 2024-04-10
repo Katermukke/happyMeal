cada6fd19788c6ceecb76e6bbe2c2384b35959aa
AdamC92 <adam.cherkani@outlook.fr>
2024-04-10 16:10:10 +0200

    localstoragejsaddfavorites

--- recipes.js
+++ recipes.js
@@ -101,3 +101,37 @@ function attachEventListeners() {
 
         
 attachEventListeners();
+
+function attachFavEvent() {
+    let addFavBtn = document.querySelectorAll('.addFav');
+    addFavBtn.forEach(button => {
+        button.addEventListener('click', function() {
+
+            const parentRecipes = this.parentNode;
+            const recipeId = parentRecipes.querySelector('.recipeId').value;
+            const idJson = {
+                id : recipeId
+            };
+
+            let existingFavList = localStorage.getItem('favRecipes');
+            if (!existingFavList) {
+                existingFavList = [];
+            } else {
+                existingFavList = JSON.parse(existingFavList);
+            }
+
+            const existingRecipeIndex = existingFavList.findIndex(item => item.id === recipeId);
+
+            if (existingRecipeIndex === -1) {
+                existingFavList.push(idJson);
+                this.classList.add('colored');
+            } else {
+                existingFavList.splice(existingRecipeIndex, 1);
+                this.classList.remove('colored');
+                
+            }
+
+            localStorage.setItem('favRecipes', JSON.stringify(existingFavList));
+        });
+    });
+}
